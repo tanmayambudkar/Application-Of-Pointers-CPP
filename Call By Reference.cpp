e09d56be297c24b8b73b92f255eccb331edebecc
Tanmay Ambudkar <tanmayambudkar@gmail.com>
2025-08-20 09:26:49 +0530

    Add files via upload

---
+++ Call By Reference.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+void swap(int *x, int *y){
+    int temp=*x;
+    *x= *y;
+    *y= temp;
+}
+
+int main(){
+    int x,y;
+    cout<<"Enter the Value of X: ";
+    cin>>x;
+    cout<<"Enter the Value of Y: ";
+    cin>>y;
+    cout<<"Value before Swap: X= "<<x<<", Y= "<<y;
+    swap(&x,&y);
+    cout <<"\nValue after Swap: X= " <<x<< ", Y= " <<y;
+    return 0;
+}
